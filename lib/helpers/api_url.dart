cdddddb2fa6f358fb28b956688d978450c019f96
nopalempl <naufalalthafi3@gmail.com>
2023-10-25 13:50:44 +0700

    pertemuan 5

--- lib/helpers/api_url.dart
+++ lib/helpers/api_url.dart
@@ -1,5 +1,5 @@
 class ApiUrl {
-  static const String baseUrl = 'http://192.168.1.9/toko-api/public';
+  static const String baseUrl = 'http://localhost/toko-api/public';
 
   static const String registrasi = baseUrl + '/registrasi';
   static const String login = baseUrl + '/login';
